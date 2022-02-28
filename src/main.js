6170a740da183e9d3114a18ec2fb1796a5eef423
Chabloz Nicolas (SEC-GC) <nicolas.chabloz@etat.ge.ch>
2022-02-28 10:38:46 +0100

    rquire all components by default

--- src/main.js
+++ src/main.js
@@ -1,12 +1,1 @@
-import './components/animate-rotation';
-import './components/cursor-listener';
-import './components/hover-highlighter';
-import './components/toggle-events';
-import './components/listen-to';
-
-import './components/emit-when-near';
-import './components/on-event-set';
-import './components/im-box';
-
-
-import './components/a-test';
+require.context('./components', false, /\.js$/);
